ae99b995fd9ddf0c7cf34aadebcde51294b0b997
Martin Boer <martin@myparcel.com>
2018-02-16 03:15:08 +0100

    :zap: live reloading

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 const gulp = require('gulp')
 const browserSync = require('browser-sync').create()
 const json_refs = require('gulp-json-refs')
+const JsonRefs = require('json-refs')
 const rename = require('gulp-rename')
 const replace = require('gulp-replace')
 
 gulp.task('bundle', function () {
+  JsonRefs.clearCache()
   return gulp.src('schema.json')
     .pipe(json_refs())
     // TODO: remove (probably all) replace functions below when OpenAPI 3.0 is merged (also clean docker-compose and env.dist)
@@ -14,24 +16,25 @@ gulp.task('bundle', function () {
     .pipe(replace('$base_url', process.argv[8] + '://' + process.argv[10]))
     .pipe(rename('swagger.json'))
     .pipe(gulp.dest('dist/'))
-    .pipe(browserSync.stream())
 })
 
 gulp.task('src', function () {
-  gulp.src(['src/**/*', '!src/index.html'])
+  return gulp.src(['src/**/*', '!src/index.html'])
     .pipe(gulp.dest('dist/'))
-  gulp.src(['src/index.html'])
+})
+
+gulp.task('html', function () {
+  return gulp.src('src/index.html')
     .pipe(replace('{{specification-json-url}}', (process.argv[6] !== undefined) ? process.argv[6] + '?d=' + Date.now() : 'http://petstore.swagger.io/v2/swagger.json'))
     .pipe(gulp.dest('dist/'))
-    .pipe(browserSync.stream())
 })
 
 gulp.task('swagger-ui', function () {
-  gulp.src(['node_modules/swagger-ui-dist/swagger-*.css', 'node_modules/swagger-ui-dist/swagger-*.js'])
+  return gulp.src(['node_modules/swagger-ui-dist/swagger-*.css', 'node_modules/swagger-ui-dist/swagger-*.js'])
     .pipe(gulp.dest('dist/swagger-ui/'))
 })
 
-gulp.task('serve', ['bundle', 'src', 'swagger-ui'], function () {
+gulp.task('serve', ['bundle', 'src', 'html', 'swagger-ui'], function () {
   browserSync.init({
     https: true,
     online: false,
@@ -44,7 +47,10 @@ gulp.task('serve', ['bundle', 'src', 'swagger-ui'], function () {
   })
 })
 
+gulp.task('bundle-sync', ['bundle'], function () {
+  browserSync.reload()
+})
+
 gulp.task('watch', ['serve'], function () {
-  gulp.watch(['specification/**/*'], {interval: 500}, ['bundle'])
-  gulp.watch(['src/**/*'], {interval: 500}, ['src'])
+  gulp.watch(['specification/**/*'], {interval: 500}, ['bundle-sync'])
 })
