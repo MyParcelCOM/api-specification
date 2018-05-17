45f080fb3bd12aa244a8dc04c0d269bc5136489a
Sam de Vries <sam@myparcel.com>
2018-05-17 11:37:27 +0200

    :bug: Fixed mp setup command not working without existing node modules.

--- mp.sh
+++ mp.sh
@@ -9,7 +9,6 @@ set -o allexport
   export $(cat ${ROOT_DIR}/.env | xargs)
   COMPOSE="docker-compose --project-name ${PROJECT_NAME}"
   RUNNING=$(${COMPOSE} ps -q)
-  [ "${RUNNING}" != "" ] && DO="exec" || DO="run --rm"
 }
 set +o allexport
 
