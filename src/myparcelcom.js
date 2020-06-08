7a95f4be081d4a42b20e08352d53c7b9e0d27fc7
Martin Boer <martin@myparcel.com>
2020-06-08 20:00:40 +0200

    :zap: validation and navigation

--- src/myparcelcom.js
+++ src/myparcelcom.js
@@ -1,20 +1,35 @@
 (function () {
-  document.addEventListener('DOMContentLoaded', function () {
-    // Wait for the swagger.json to be processed.
-    var swaggerInterval = setInterval(function () {
-      var ready = document.getElementsByClassName('api-content')[0]
-      if (typeof ready !== 'undefined') {
-        clearInterval(swaggerInterval)
+  // Redoc options: https://github.com/Redocly/redoc#redoc-options-object
+  Redoc.init(
+    'dist/swagger.json',
+    {
+      expandSingleSchemaField: true,
+      hideDownloadButton: true,
+      jsonSampleExpandLevel: 'all',
+      lazyRendering: true
+    },
+    document.getElementById('redoc'),
+    function () {
+      requestAnimationFrame(function () {
+        // Scroll active menu item into view.
+        if (window.location.hash) {
+          var activeLabel = document.querySelector('label.active')
 
+          if (activeLabel.parentNode.parentNode.hasAttribute('role')) {
+            activeLabel.scrollIntoView()
+          } else {
+            activeLabel.parentNode.parentNode.parentNode.scrollIntoView()
+          }
+        }
         // Open external information links in a new tab.
-        var links = document.querySelectorAll('.api-info a');
+        var links = document.querySelectorAll('.api-info a')
 
         for (var i = 0, linksLength = links.length; i < linksLength; i++) {
           if (links[i].href.substr(0, 4) === 'http' && links[i].hostname !== window.location.hostname) {
-            links[i].target = '_blank';
+            links[i].target = '_blank'
           }
         }
-      }
-    }, 137)
-  })
+      })
+    }
+  )
 })()
