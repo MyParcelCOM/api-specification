cea9000408196816ec007b40ad22f754b5355e62
Martin Boer <martin@myparcel.com>
2020-06-09 12:57:20 +0200

    :zap: experimental and internal endpoints

--- src/myparcelcom.js
+++ src/myparcelcom.js
@@ -11,22 +11,50 @@
     document.getElementById('redoc'),
     function () {
       requestAnimationFrame(function () {
-        // Scroll active menu item into view.
-        if (window.location.hash) {
-          var activeLabel = document.querySelector('label.active')
+        // Open external links in a new tab.
+        var links = document.querySelectorAll('.api-content a')
 
-          if (activeLabel.parentNode.parentNode.hasAttribute('role')) {
-            activeLabel.scrollIntoView()
-          } else {
-            activeLabel.parentNode.parentNode.parentNode.scrollIntoView()
+        for (var l = 0; l < links.length; l++) {
+          if (links[l].href.substr(0, 4) === 'http' && links[l].hostname !== window.location.hostname) {
+            links[l].target = '_blank'
           }
         }
-        // Open external information links in a new tab.
-        var links = document.querySelectorAll('.api-info a')
 
-        for (var i = 0, linksLength = links.length; i < linksLength; i++) {
-          if (links[i].href.substr(0, 4) === 'http' && links[i].hostname !== window.location.hostname) {
-            links[i].target = '_blank'
+        // Mark experimental endpoints.
+        var experimental = [
+          'HookLogs',
+          'Users',
+          'Webhooks'
+        ]
+        for (var x = 0; x < experimental.length; x++) {
+          var elements = document.querySelectorAll([
+            '[id^="tag/' + experimental[x] + '"]'
+          ].join(','))
+
+          for (var e = 0; e < elements.length; e++) {
+            elements[e].classList.add('experimental')
+          }
+        }
+
+        // Hide internal endpoints.
+        var internal = [
+          'CombinedFiles',
+          'Invitations',
+          'Invoices',
+          'PasswordResets',
+          'Payments',
+          'PaymentIdentities',
+          'ShipmentSurcharges',
+          'SystemMessages'
+        ]
+        for (var i = 0; i < internal.length; i++) {
+          var sections = document.querySelectorAll([
+            '[data-item-id="tag/' + internal[i] + '"]',
+            '[id^="tag/' + internal[i] + '"]'
+          ].join(','))
+
+          for (var s = 0; s < sections.length; s++) {
+            sections[s].remove()
           }
         }
       })
